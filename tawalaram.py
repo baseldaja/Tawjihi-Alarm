57e39e491c5bf03b170ce34524b9bed6cc15c750
Basel F. Al-Dajah <45013854+baseldaja@users.noreply.github.com>
2024-08-08 13:58:26 +0300

    Update tawalaram.py

--- tawalaram.py
+++ tawalaram.py
@@ -9,18 +9,27 @@ def check_website(url):
         return False
 
 def main():
-    website_url = "https://www.tawjihi.jo/?fbclid=IwAR2xqr6CdO5-SrQ6Jf1lFN8OSTIDWKhjp3a3uN_h9BbSnfC4gvNhRKsfOAc"
+    website_url = "https://www.tawjihi.jo/"
     check_interval = 10
 
     website_live = False
 
     while True:
-        while not website_live and not check_website(website_url):
-            print("Website is not live. Checking again in {} seconds.".format(check_interval))
-            time.sleep(check_interval)
-
         if not website_live:
-            print("Website is live!")
-            website_live = True
+            if check_website(website_url):
+                print("Website is live!")
+                website_live = True
+            else:
+                print(f"Website is not live. Checking again in {check_interval} seconds.")
+        else:
+            if not check_website(website_url):
+                print("Website has gone down.")
+                website_live = False
+            else:
+                print(f"Website is still live. Checking again in {check_interval} seconds.")
 
-        time.sleep(check_interval) 
+        time.sleep(check_interval)
+
+if __name__ == "__main__":
+    main()
+
